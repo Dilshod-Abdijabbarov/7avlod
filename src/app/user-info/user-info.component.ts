0cd0c34d1cbcb4d840e9a9369c0ff0bf5cf8abb6
Dilshod Abdijabbarov <94256812+Dilshod-Abdijabbarov@users.noreply.github.com>
2026-04-23 17:09:43 +0500

    folder changed

--- src/app/user-info/user-info.component.ts
+++ src/app/user-info/user-info.component.ts
@@ -13,7 +13,9 @@ export class UserInfoComponent {
     imageUrl: 'https://via.placeholder.com/150'
   };
 
+  fallbackImageUrl = 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxMjAgMTIwIj48Y2lyY2xlIGN4PSI2MCIgY3k9IjYwIiByPSI2MCIgZmlsbD0iIzQ4OThlMiIgLz48cGF0aCBmaWxsPSIjZmZmIiBkPSJNNDkuOTg4IDQxLjVhMTguNSA4IDEgMCAxIDM3IDEuNzUgMTguNSAxOC41IDAgMCAxLTM3LTEuNzV6bTAtLjVhMTkuNSAxOS41IDAgMCAwIDM5IDAgMTkuNSAxOS41IDAgMCAwLTM5IDB6TTM1LjUgODIuNThjMC0xNy4xNCAxMi43LTE4LjUgMjQuNS0xOC41IDEwLjIgMCAyMy42IDEuODQgMjUuNSA4LjUgOS45IDEyLjUgMTIuNSAyMy41IDEyLjUgMzMuMEM5OC4zIDg4LjkgOTIuMDkgODAgNzAgODAgNTAuMSA4MCAzNS41IDY4LjUgMzUuNSA4Mi41OHoiIC8+PC9zdmc+';
+
   onImageError(event: any) {
-    event.target.src = 'https://via.placeholder.com/150?text=User';
+    event.target.src = this.fallbackImageUrl;
   }
 }
